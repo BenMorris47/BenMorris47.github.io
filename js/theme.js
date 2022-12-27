a96e2b61353d51b95600e8864961c912bc57f42a
Ben Morris <bm248766@falmouth.ac.uk>
2022-12-27 16:18:31 +0000

    Fixed grammer and issues lewis raised

--- js/theme.js
+++ js/theme.js
@@ -250,7 +250,7 @@ $(".counter").each(function () {
 $(".typed").each(function() {
 var typed = new Typed('.typed', {
     stringsElement: '.typed-strings',
-	loop: true,
+	loop: false,
 	typeSpeed: 100,
     backSpeed: 50,
 	backDelay: 1500,
